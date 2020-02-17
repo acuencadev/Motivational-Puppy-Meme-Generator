caf61a4099e6bf931b6ad1ca4f888ba54b1bfa70
Amador Cuenca <sphi02ac@gmail.com>
2020-02-17 18:51:40 -0300

    fix: Fix the PDFIngestor implementation

--- src/QuoteEngine/PDFIngestor.py
+++ src/QuoteEngine/PDFIngestor.py
@@ -1,32 +1,37 @@
 import os
 import subprocess
-from random import randint
 from typing import List
+from uuid import uuid4
 
 from .IngestorInterface import IngestorInterface
 from .QuoteModel import QuoteModel
 
 
-class DocxIngestor(IngestorInterface):
+class PDFIngestor(IngestorInterface):
     allowed_extensions = ['pdf']
     
     @classmethod
     def parse(cls, path: str) -> List[QuoteModel]:
         if not cls.can_ingest(path):
             raise Exception("Could not parse the selected file.")
-        
+                
         quotes = []
         
-        tmp = f"./tmp/{randint(0, 100000000)}.txt"
-        call = subprocess.call(['pdftotext', path, tmp])
+        tmp = f"./tmp/{uuid4()}.txt"
+        
+        call = subprocess.call(['pdftotext',
+                                path,
+                                tmp])
         
         with open(tmp, 'r') as fin:
             for line in fin.readlines():
-                line - line.strip('\n\r').strip()
+                line = line.strip('\n\r').strip()
                 
                 if len(line) > 0:
                     parse = line.split(' - ')
                     quote = QuoteModel(body=parse[0], author=parse[1])
                     quotes.append(quote)
+                    
+        os.remove(tmp)
         
         return quotes
